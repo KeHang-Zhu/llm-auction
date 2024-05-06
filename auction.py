7803a01f47710d37922c24b53b81209c6f49b6cb
KeHang-Zhu <kehangzhu@gmail.com>
2024-05-06 16:15:12 -0400

    created auction

--- auction.py
+++ auction.py
@@ -13,9 +13,9 @@ agents = [
     Agent(name="John", instruction ="you are bidder 1"),
     Agent(name="Robin", instruction="you are bidder 2")
     ]
-rule = Rule('open', 'ascending', 'common value')
+rule = Rule('open', 'private','ascending', 'common value')
 auction = Auction(agents, rule=rule)
-auction.run(n_rounds=10, temperature=0.4)
+auction.run(temperature=0.4)
 
 
 c.write_jsonl("conversation_cache.jsonl")
