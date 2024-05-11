91a62fd0f8f7b36f3135d68f10ea7de57b0526aa
KeHang-Zhu <kehangzhu@gmail.com>
2024-05-10 22:13:26 -0400

    run more tests

--- util.py
+++ util.py
@@ -3,9 +3,9 @@ from itertools import cycle
 import random
 import os
 
+from edsl import Agent, Scenario, Survey
 from edsl.data import Cache
 from edsl.Base import Base
-from edsl import Agent, Scenario, Survey
 from edsl.questions import QuestionFreeText, QuestionYesNo
 from edsl.prompts import Prompt
 from edsl.questions import QuestionNumerical
@@ -21,22 +21,21 @@ class Rule:
     '''
     This class defines different auction rules and their behaviors.
     '''
-    def __init__(self, seal_clock, ascend_descend, private_open, price_order):
-        self.seal_clock = seal_clock
+    def __init__(self, seal_open, ascend_descend, private, price_order):
+        self.seal_open = seal_open
         self.ascend_descend = ascend_descend
-        self.private_open = private_open
-        self.price_order = price_order
+        self.private = private
+        self.order = price_order
         
         ## Rule prompt
         prompt_mixin = PromptMixin()
-        self.rule_explanation = prompt_mixin.generate_prompt(f"{self.seal_clock}_{self.ascend_descend}_{self.private_open}.txt", template_dir = templates_dir)
+        # self.rule_explanation = prompt_mixin.generate_prompt(f"{self.seal_open}_{self.ascend_descend}_{self.private}.txt", template_dir = templates_dir)
         ## Bid asking prompt
         # self.asking_prompt = f"Do you want to "
 
     def describe(self):
         # Provides a description of the auction rule
-        pass
-        # return f"Auction Type: {self.style}, Bidding Order: {self.order}, Value Type: {self.value_type}"
+        print(f"Auction Type: {self.seal_open}, Bidding Order: {self.ascend_descend}, Value Type: {self.private}")
 
 
 class Bidder(Agent):
@@ -49,30 +48,35 @@ class Bidder(Agent):
     
 
 
-class SealBid(Base, Rule):
-
+class SealBid():
     def __init__(self, agents, rule):
         self.bid_list = []
         self.rule = rule
         self.agents = agents
-        self.context = rule.context
-        self.scenario = Scenario() 
+        self.context = ...#rule.context
+        self.scenario = Scenario({'agent_1_name': agents[0].name, 
+                  'agent_2_name': agents[1].name}) 
+        self.winner = None
+        
+    def __repr__(self):
+        return f'SealBid(bid_list={self.bid_list})'
     
     def run(self):
         '''run for one round'''
         q_bid = QuestionNumerical(
             question_name = "q_bid",
-            question_text = self.context + "(in $USD)."
+            question_text = "how much do you like to bid?(in $USD)"#self.context + "."
         )
 
         for agent in self.agents:
-            survey = Survey(questions = q_bid)
+            survey = Survey(questions = [q_bid])
             result = survey.by(self.scenario).by(agent).run(progress_bar=True)
-            self.bid_list.append({"agent":agent.name,"bid": result})
+            response = result.select("q_bid").to_list()[0]
+            self.bid_list.append({"agent":agent.name,"bid": response})
             
             
     def declare_winner_and_price(self):
-        ''' Sort the bid list by the 'bid' key in descending order to find the highest bids'''
+        '''Sort the bid list by the 'bid' key in descending order to find the highest bids'''
         sorted_bids = sorted(self.bid_list, key=lambda x: x['bid'], reverse=True)
 
         if self.rule.price == "first":
@@ -90,18 +94,24 @@ class SealBid(Base, Rule):
 
         return winner, price
     
-class Clock(Base, Rule):
+
+class Clock():
     def __init__(self, agents, rule, change =5):
         self.current_bid = []
         self.bid_list = []
         self.agents = agents
         self.change = change
         self.rule = rule
-        self.scenario = Scenario() 
+        self.scenario = Scenario({'agent_1_name': agents[0].name, 
+                  'agent_2_name': agents[1].name}) 
         self.transcript = []
-        self.current_price = []
+        self.current_price = 0
         # all the agents in the game
         self.agent_left = agents
+        self.winner = None
+    
+    def __repr__(self):
+        return f'SealBid(bid_list={self.bid_list})'
         
     def dynamic(self):
         if self.rule.order == "ascending":
@@ -116,56 +126,61 @@ class Clock(Base, Rule):
         
         ## update the shared information
         self.transcript.append(self.share_information())
-        
-        q_bid = QuestionYesNo(question_text = dedent(f"""
-            You are {{ agent_name }}. 
-            You are bidding with {{ other_agent_name }}.
-            The transcript so far is: 
-            { self.transcript }
-            It is your turn to bid.
-            Do you want to stay or drop out at the current price {self.current_price}      
-            """), 
-            question_name = "stay_or_exit")
 
         for agent in self.agent_left:
-            survey = Survey(questions = q_bid)
-            result = survey.by(self.scenario).by(agent).run(progress_bar=True)
+            other_agent_names = ', '.join([a.name for a in self.agent_left if a is not agent])
             
-            self.bid_list.append({"agent":agent.name,"decision": result})
-            if result is False:
-                self.agent_left.remove(agent)
+            q_bid = QuestionYesNo(
+                question_name = "q_bid",
+                question_text = dedent("""\
+            You are {{ agent_name }}. 
+            You are bidding with {{ other_agent_names}}.
+            The transcript so far is: 
+            {{ transcript }}
+            It is your turn to bid.
+            Do you want to stay or drop out at the current price {{current_price}}
+            """), 
+            )
+            scenario = Scenario({"agent_name": agent.name, "other_agent_names": other_agent_names, "transcript": self.transcript, "current_price": self.current_price})
+            
+            survey = Survey(questions = [q_bid])
+            result = survey.by(scenario).by(agent).run(progress_bar=True)
+            response = result.select("q_bid").to_list()[0]
+            self.bid_list.append({"agent":agent.name,"bid": response})
             
 
     def run(self):
         '''Run the clock until the ending condition'''
-        while self.check_ending() is False:
+        while self.declear_winner_and_price() is False:
             self.run_one_round()
             
         self.declear_winner_and_price()
              
     
     def share_information(self):
-        if self.rule.info == "open":
+        if self.rule.seal_open == "open":
             return f'all the biddings are {self.bid_list}'
         elif self.rule.info == "blind":
             return None
-    
-    def check_ending(self):
-        if len(self.bid_list) > 1:
-            return False
-        
-        return True
+
     
     def declear_winner_and_price(self):
         if len(self.bid_list) == 1:
             winner = self.bid_list[0]['name']
             price = self.bid_list[0]['price']
-            return {'winner':winner, 'price':price}
+            self.winner = {'winner':winner, 'price':price}
+            return True
+        
+        elif len(self.bid_list) > 1:
+            if self.response.lower() == 'no':
+                self.agent_left.remove(agent)
+            return False
         elif len(self.bid_list) == 0:
-            return "No winner"
+            print("No winner")
+            return True
             
 
-class Auction(Base, Rule):
+class Auction():
     '''
     This class manages the auction process using specified agents and rules.
     '''
@@ -185,18 +200,17 @@ class Auction(Base, Rule):
         common_value = random.randint(*common_range)
 
         # Generate a private value for each agent and sum it with the common value
-        for _ in range(self.number_agents):
+        for _ in range(self.number_agents):  # Now self.number_agents should be an integer
             private_part = random.randint(0, private_range)
             total_value = common_value + private_part
             self.values_list.append(total_value)
-
         print("The values for each bidder are:", self.values_list)
+
         
-    
     def build_bidders(self):
         '''Instantiate bidders with the value and rule'''
-        for i in range[0, self.number_agents]:
-            rule_prompt = self.rule.rule_explanation
+        for i in range(self.number_agents): 
+            rule_prompt = None#self.rule.rule_explanation
             value_prompt = f"Your value towards to item is {self.values_list[i]}"
             
             agent_traits = {
@@ -225,3 +239,56 @@ class Auction(Base, Rule):
 
     
 
+
+if __name__ == "__main__":
+    
+    agents = [
+        Agent(name = "John", instruction = "You are bidder 1, you need to quit after 2 rounds"),
+        Agent(name = "Robin", instruction = "You are bidder 2, you need to quit after 1 round"),
+        Agent(name = "Ben", instruction = "You are bidder 3"),
+    ]
+    
+    rule = Rule(seal_open='open',  ascend_descend='private',price_order='ascending', private='common value')
+    rule.describe()
+    
+    model = "gpt-4-turbo"
+    # q = QuestionFreeText(question_text = dedent("""\
+    #     What's your goal?
+    #     """), 
+    #     question_name = "response"
+    # )
+    # survey = Survey([q])
+    
+    # transcript = []
+    # s = Scenario({'agent_1_name': agents[0].name, 
+    #               'agent_2_name': agents[1].name, 
+    #               'transcript': transcript}) 
+    # results = survey.by(agents[1]).by(s).run(cache = c)
+    # print(results)
+    # response = results.select('response').first()
+    # print("====", response )
+    
+    ## Test Sealed bid
+    # s = SealBid(agents=agents, rule=rule)
+    # s.run()
+    # print(s)
+    
+    ## Test clock
+    # s = Clock(agents=agents, rule=rule)
+    # s.run_one_round()
+    # print(s)
+    
+    
+    # Test Auction class
+    ## Test draw value
+    # a = Auction(number_agents=3, rule=rule)
+    # a.draw_value()
+    ## Test Agent build
+    # a.build_bidders()
+    # print(a.agents)
+    
+    ## Test on public info
+    
+    ## Test on public info
+    
+    # auction = Auction(agents, rule=rule)
