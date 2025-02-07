640090b24b1f38cb6bd88ad337154a9b5bd86df3
Kehang Zhu <kehangzh@G94QN726NH.tld>
2025-02-06 23:54:06 -0500

    add the new ac and ac-b data

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def run_auction(i, human, number_agents, rule, output_dir, c):
         a = Auction_ebay(number_agents=number_agents, rule=rule, output_dir=output_dir, timestring=timestring, cache=c, model='gpt-4o', temperature=0.5)
     else:
         a = Auction_plan(number_agents=number_agents, rule=rule, output_dir=output_dir, timestring=timestring, cache=c, model='gpt-4o', temperature=0.5)
-    a.draw_value(seed=1399 + i)
+    a.draw_value(seed=1404 + i)
     a.run_repeated()
     c.write_jsonl(os.path.join(output_dir, f"raw_output__{timestring}.jsonl"))
 
@@ -30,16 +30,16 @@ if __name__ == "__main__":
     ascend_descend = 'ascend'
     price_order = 'second'
     private_value = 'affiliated'  ### private value / common value / affiliated value
-    open_blind = 'open'            ### In AC, whether the information is closed or open
+    open_blind = 'blind'            ### In AC, whether the information is blind or open
     number_agents = 3
     human = False                  ## Humanistic prompt or not
     ebay = False                    ## If it's eBay auction
-    round = 4
+    round = 10
     turns = 10
     closing = True
     reserve_price = 60
     
-    output_dir = f"experiment_logs/V10/affiliated_ac"
+    output_dir = f"experiment_logs/V10/affiliated_ac_closed"
     # {seal_clock}_{ascend_descend}_{price_order}_{private_value}_{open_blind}"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -51,19 +51,21 @@ if __name__ == "__main__":
         open_blind=open_blind, 
         rounds=round, turns=turns , common_range=[0, 20], private_range=20, increment=1, 
         number_agents=number_agents,
-        special_name="affiliated_ac.txt",
+        special_name="affiliated_ac_closed.txt",
         closing = closing,
         reserve_price = reserve_price)
     rule.describe()
 
-    N = 1 # Repeat for N times
+    N = 5 # Repeat for N times
 
-    
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(run_auction, i, human, number_agents, rule, output_dir, c) for i in range(N)]
+    for i in range(N):
+        run_auction( i, human, number_agents, rule, output_dir, c)
 
-    for future in concurrent.futures.as_completed(futures):
-        try:
-            future.result()
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     futures = [executor.submit(run_auction, i, human, number_agents, rule, output_dir, c) for i in range(N)]
+
+    # for future in concurrent.futures.as_completed(futures):
+    #     try:
+    #         future.result()
+    #     except Exception as e:
+    #         print(f"An error occurred: {e}")
